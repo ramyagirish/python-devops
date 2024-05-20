9a29bf74175f7797487d0cf2dd6e3eeeb611a30a
ramyagirish <girish.sureshkumar@gmail.com>
2024-05-20 23:43:56 +0000

    add linting

---
+++ devops_lib/random_fruit.py
@@ -0,0 +1,5 @@
+from random import choices
+
+def fruit():
+    fruits = ["apple", "cheery", "banana", "strawberry"]
+    return choices(fruits)[0]
---
+++ hello.py
@@ -0,0 +1,6 @@
+from devops_lib.random_fruit import fruit
+
+for i in range(4):
+    print(f"This is {i+1}th chance")
+    print(fruit())
+
